d7b758019dfbf39f79d2dfec259ad7bdbe67fa76
nishank <nishank_3@outlook.com>
2026-09-05 19:35:12 +0530

    pycalcv1.1

--- calculatorv1.py
+++ calculatorv1.py
@@ -1,39 +1,44 @@
 #!/usr/bin/python3
-print("welcome to pycalc!!!")
-print("use the command exit in operation prompt to exit pycalc")
-print("use the command reset in operation prompt to restart pycalc")
-num_1 = int(input("enter first number: "))
+try:    
+    print("welcome to pycalc!!!")
+    print("use the command exit in operation prompt to exit pycalc")
+    print("use the command reset in operation prompt to restart pycalc")
+    num_1 = int(input("enter first number: "))
 
-while True:
-
-
-    operation = str(input("type operation symbol: "))
-    result = 0
-
-    if operation == "exit":
-        break
-
-    if operation == "reset":
-        num_1 = int(input("enter first number: "))
-        continue
-    num_2 = int(input("enter second number: "))
+    while True:
 
 
-    if operation == "+":
-        result =  num_1 + num_2
+        operation = str(input("type operation symbol: "))
+        result = 0
 
-    if operation == "-":
-        result =  num_1 - num_2
+        if operation == "exit":
+            break
 
-    if operation == "*":
-        result =  num_1 * num_2
+        if operation == "reset":
+            num_1 = int(input("enter first number: "))
+            continue
+        num_2 = int(input("enter second number: "))
 
-    if operation == "/":
-        result =  num_1 / num_2
 
-    if operation == "%":
-        result =  num_1 % num_2
+        if operation == "+":
+            result =  num_1 + num_2
 
-    print(result)
+        if operation == "-":
+            result =  num_1 - num_2
 
-    num_1 = result
+        if operation == "*":
+            result =  num_1 * num_2
+
+        if operation == "/":
+            result =  num_1 / num_2
+
+        if operation == "%":
+            result =  num_1 % num_2
+
+        print(result)
+
+        num_1 = result
+except ValueError:
+    print("please type a valid operation/number")
+except ZeroDivisionError:
+    print("you cant divide by zero")
