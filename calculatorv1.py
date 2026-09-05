7dc1671cb0ed07ddb02b7f1c4cbb6ea94022f7cd
nishank <nishank_3@outlook.com>
2026-09-05 13:20:39 +0530

    pycalc v1.0

---
+++ calculatorv1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+num_1 = int(input("enter first number: "))
+
+while True:
+
+
+    operation = str(input("type operation symbol: "))
+    result = 0
+
+    if operation == "exit":
+        break
+
+    if operation == "reset":
+        num_1 = int(input("enter first number: "))
+        continue
+    num_2 = int(input("enter second number: "))
+
+
+    if operation == "+":
+        result =  num_1 + num_2
+
+    if operation == "-":
+        result =  num_1 - num_2
+
+    if operation == "*":
+        result =  num_1 * num_2
+
+    if operation == "/":
+        result =  num_1 / num_2
+
+    if operation == "%":
+        result =  num_1 % num_2
+
+    print(result)
+
+    num_1 = result
