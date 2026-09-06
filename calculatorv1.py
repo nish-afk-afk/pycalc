36a0fbe5fcc31c099310f6bafe5e7e3473277989
nishank <nishank_3@outlook.com>
2026-09-06 16:37:08 +0530

    pycalc v1.4.1

--- calculatorv1.py
+++ calculatorv1.py
@@ -6,7 +6,7 @@ try:
     print("use the command exit in operation prompt to exit pycalc")
     print("use the command reset in operation prompt to restart pycalc")
 
-    num_1 = int(input("enter first number: "))
+    num_1 = float(input("enter first number: "))
 
     sqrt_result = 0
 
@@ -21,22 +21,27 @@ try:
             break
 
         if operation == "reset":
-            num_1 = int(input("enter first number: "))
+            num_1 = float(input("enter first number: "))
             continue
 
         if operation == "sqrt":
-            sqrt_result = math.sqrt(num_1)
-            result = sqrt_result
+            if num_1 < 0:
+                print("you cant take negative sqrts")
+                continue
+            result = math.sqrt(num_1)
             print(result)
             num_1 = result
             continue
 
         if operation == "!":
+            if num_1 < 0:
+                print("you cant take negative factorials")
+                continue
             result = math.factorial(num_1)
             print(result)
             num_1 = result
             continue
-        num_2 = int(input("enter second number: "))
+        num_2 = float(input("enter second number: "))
 
 
         if operation == "+":
@@ -52,6 +57,9 @@ try:
             result =  num_1 / num_2
 
         if operation == "%":
+            if num_2 == 0:
+                print("you cant do a modulo of zero")
+                continue
             result =  num_1 % num_2
         
         if operation == "perc":
--- update logs.md
+++ update logs.md
@@ -1,4 +1,5 @@
 |v.1-
+
 $added basic calculator
 
 $added 5 basic operations
@@ -8,20 +9,43 @@ $added closing
 $added continuous equations
 
 
+
 |v.1.0.1
+
 $added guide text on top
 
+
+
 |v.1.1
+
 $added error handling
 
+
+
 |V.1.2
+
 $added square roots with "`sqrt`" operation
 
+
+
 |v.1.3
+
 $added factorials with "`!`" operation
 
+
+
 |v.1.3.1
+
 $fixed factorial bug
 
+
+
 |v.1.4
-$added percentages with the  "`perc`" operation 
+
+$added percentages with the  "`perc`" operation
+
+
+
+|v1.4.1
+
+$added hotfix for supporting decimals 
