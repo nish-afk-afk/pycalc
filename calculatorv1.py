e45c41c3a673382387e8e2d701801d2b0c3c955d
nishank <nishank_3@outlook.com>
2026-09-06 17:16:11 +0530

    pycalc v1.4.3

--- calculatorv1.py
+++ calculatorv1.py
@@ -18,69 +18,69 @@ except ValueError:
 
     while True:
 
-    try:
-        operation = str(input("type operation symbol: "))
-    except ValueError:
-        print("please enter a valid operation")
+        try:
+            operation = str(input("type operation symbol: "))
+        except ValueError:
+            print("please enter a valid operation")
 
-        if operation != ["+", "-", "*", "/", "sqrt", "!", "exit", "reset",]: 
-            print("that operation isnt valid or it isnt included in pycalc")  
-            num_1 = float(input("enter first number: "))
-            continue
-
-        if operation == "exit":
-            break
-
-        if operation == "reset":
-            num_1 = float(input("enter first number: "))
-            continue
-
-        if operation == "sqrt":
-            if num_1 < 0:
-                print("you cant take negative sqrts")
+            if operation != "+" or "-" or "*" or "/" or "sqrt" or "!" or "exit" or "reset": 
+                print("that operation isnt valid or it isnt included in pycalc")  
+                num_1 = float(input("enter first number: "))
                 continue
 
-            result = math.sqrt(num_1)
+            if operation == "exit":
+                break
+
+            if operation == "reset":
+                num_1 = float(input("enter first number: "))
+                continue
+
+            if operation == "sqrt":
+                if num_1 < 0:
+                    print("you cant take negative sqrts")
+                    continue
+
+                result = math.sqrt(num_1)
+                print(result)
+                num_1 = result
+                continue
+
+            if operation == "!":
+                if num_1 < 0:
+                    print("you cant take decimal/negative factorials")
+                    continue
+
+                result = math.factorial(num_1)
+                print(result)
+                num_1 = result
+                continue
+        try:    
+            num_2 = float(input("enter second number: "))
+        except ValueError:
+            print("thats not a valid number or decimal")
+
+            if operation == "+":
+                result =  num_1 + num_2
+
+            if operation == "-":
+                result =  num_1 - num_2
+
+            if operation == "*":
+                result =  num_1 * num_2
+
+            if operation == "/":
+                result =  num_1 / num_2
+
+            if operation == "%":
+                if num_2 == 0:
+                    print("you cant do a modulo of zero")
+                    continue
+                result =  num_1 % num_2
+            
+            if operation == "perc":
+                result = (num_1 / 100) * num_2
+
             print(result)
+
             num_1 = result
-            continue
-
-        if operation == "!":
-            if num_1 < 0 or not num_1 is_integer():
-                print("you cant take decimal/negative factorials")
-                continue
-
-            result = math.factorial(num_1)
-            print(result)
-            num_1 = result
-            continue
-    try:    
-        num_2 = float(input("enter second number: "))
-    except ValueError:
-        print("thats not a valid number or decimal")
-
-        if operation == "+":
-            result =  num_1 + num_2
-
-        if operation == "-":
-            result =  num_1 - num_2
-
-        if operation == "*":
-            result =  num_1 * num_2
-
-        if operation == "/":
-            result =  num_1 / num_2
-
-        if operation == "%":
-            if num_2 == 0:
-                print("you cant do a modulo of zero")
-                continue
-            result =  num_1 % num_2
-        
-        if operation == "perc":
-            result = (num_1 / 100) * num_2
-
-        print(result)
-
-        num_1 = result
     
